0ef592c3b0522786db6b8a62f4857cbdb8abf76d
Mung Nguyen <mungyp98@gmail.com>
2019-09-23 22:30:03 +0700

    layout done 23.09.19

--- KTXH_caphuyen/layouts/default.vue
+++ KTXH_caphuyen/layouts/default.vue
@@ -1,88 +1,59 @@
 <template>
-  <v-app dark>
+  <v-app class="my-app">
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
       :clipped="clipped"
+      :permanent="permanent"
+      color="rgb(159, 211, 224)"
       fixed
       app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <!-- <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn> -->
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+    ></v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app height="100" color="rgb(159, 211, 224)">
+      <!-- <v-toolbar :extended="extended"> -->
+      <v-row>
+        <v-col cols="6" class="header-left">
+          <v-row>
+            <v-col cols="5">
+              <v-img
+                :src="require('@/assets/images/logo.png')"
+                aspect-ratio="1"
+                class="logo"
+                max-width="240"
+                max-height="90"
+              ></v-img>
+            </v-col>
+            <v-col cols="7" class="header-title">
+              <b>HỆ THỐNG THÔNG TIN KINH TẾ XÃ HỘI</b>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="6" class="header-right">
+          <v-row>
+            <v-col cols="7" class="header-title">
+              <p>
+                <b>UBND HUYỆN CAM LỘC - TỈNH QUẢNG TRỊ</b>
+              </p>
+            </v-col>
+            <v-col cols="5" class="right-title header-title">
+              <p>
+                <b>PHÒNG KẾ HOẠCH - TÀI CHÍNH</b>
+              </p>
+              <P>Quản trị</P>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+      <!-- </v-toolbar> -->
+      <!-- <v-toolbar-title v-text="title" />
+      <v-spacer />-->
     </v-app-bar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
+    <v-footer :fixed="fixed" app>
       <span>&copy; 2019</span>
     </v-footer>
   </v-app>
@@ -90,28 +61,65 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      clipped: false,
+      clipped: true,
+      permanent: true,
       drawer: false,
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/"
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          icon: "mdi-chart-bubble",
+          title: "Inspire",
+          to: "/inspire"
         }
       ],
+      extended: true,
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
-    }
+      title: "Vuetify.js"
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.logo {
+  .v-image__image {
+    margin: 5% 0;
   }
 }
-</script>
+
+.header-left {
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  .header-title {
+    padding-top: 3%;
+  }
+}
+
+.header-right {
+  *,
+  p {
+    margin: 0;
+    padding: 0;
+  }
+
+  .header-title {
+    padding-top: 6%;
+  }
+
+  .right-title {
+    text-align: right;
+  }
+}
+</style>
