00cbddabbbdbc16ab6b3fcc2f1171579391e99f1
Quang <tienquang98tb@gmail.com>
2019-11-17 03:05:58 +0700

    .

--- KTXH_caphuyen/components/Bars/userBar1.js
+++
@@ -1,9 +0,0 @@
-export default [
-    { text: "My Files", icon: "mdi-folder" },
-    { text: "Shared with me", icon: "mdi-account-multiple" },
-    { text: "Starred", icon: "mdi-star" },
-    { text: "Recent", icon: "mdi-history" },
-    { text: "Offline", icon: "mdi-check-circle" },
-    { text: "Uploads", icon: "mdi-upload" },
-    { text: "Backups", icon: "mdi-cloud-upload" }
-]
