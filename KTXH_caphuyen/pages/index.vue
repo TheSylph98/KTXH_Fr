5a79b1e33007846de822b0dd61d48498e91a79ea
Mung Nguyen <mungyp98@gmail.com>
2019-11-30 10:12:10 +0700

    delete test in index

--- KTXH_caphuyen/pages/index.vue
+++ KTXH_caphuyen/pages/index.vue
@@ -3,21 +3,11 @@
     <h1>Quản lý kinh tế - xã hội</h1>
 
     <span>{{ new Date() | formatDate }}</span>
-
-    <DonViDiaBan />
   </div>
 </template>
 
 <script>
-import StringFilter from "@/components/Filter/StringFilter";
-import DonViDiaBan from "../components/Dialog/Quantri/DonViDiaBan";
-
 export default {
-  components: {
-    StringFilter,
-    DonViDiaBan
-  },
-
   data: () => ({
     desserts: [
       {
